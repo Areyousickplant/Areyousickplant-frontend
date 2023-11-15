d1e81583b661187cd24754a01ac63a5087cde08d
jyh071116 <jyh071116@gmail.com>
2023-11-15 14:49:58 +0900

    feat: 폴더구조 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "pages/Home";
+import Detail from "pages/Detail";
+import Login from "pages/Login";
+import Signup from "pages/Signup";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   );
